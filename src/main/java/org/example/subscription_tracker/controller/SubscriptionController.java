ef6ec70ea10fbe8d94a255e9b9a1227295ab3584
Ksenia <kseniasjava@gmail.com>
2025-06-24 22:15:26 +0300

    feat: implement REST controller with full CRUD endpoints

---
+++ src/main/java/org/example/subscription_tracker/controller/SubscriptionController.java
@@ -0,0 +1,44 @@
+package org.example.subscription_tracker.controller;
+
+import org.example.subscription_tracker.entity.Subscription;
+import org.example.subscription_tracker.service.SubscriptionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class SubscriptionController {
+
+    @Autowired
+    private SubscriptionService subscriptionService;
+
+    @GetMapping("/subscriptions")
+    public List<Subscription> getAllSubscriptions() {
+        return subscriptionService.findAll();
+    }
+
+    @GetMapping("/subscriptions/{id}")
+    public Subscription getByIdSubscriptions(@PathVariable Long id) {
+        return subscriptionService.findById(id)
+                .orElseThrow(() -> new RuntimeException("Subscription with ID " + id + " not found"));
+    }
+
+    @PostMapping("/subscriptions")
+    public Subscription saveSubscription(@RequestBody Subscription subscription) {
+        return subscriptionService.save(subscription);
+    }
+
+    @PutMapping("/subscriptions/{id}")
+    public Subscription updateSubscription(@RequestBody Subscription subscription, @PathVariable Long id) {
+        return subscriptionService.update(subscription, id);
+    }
+
+    @DeleteMapping("/subscriptions/{id}")
+    public void deleteSubscription(@PathVariable Long id) {
+        Subscription subscription = subscriptionService.findById(id)
+                .orElseThrow(() -> new RuntimeException("Subscription with ID " + id + " not found"));
+        subscriptionService.delete(subscription);
+    }
+}
---
+++ src/main/java/org/example/subscription_tracker/repository/SubscriptionRepository.java
@@ -0,0 +1,4 @@
+package org.example.subscription_tracker.repository;
+
+public class SubscriptionRepository {
+}
