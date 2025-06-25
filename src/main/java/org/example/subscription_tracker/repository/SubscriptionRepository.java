252c3587e6e38fb877fc9f3971e0cc7a6d398972
Ksenia <kseniasjava@gmail.com>
2025-06-25 20:25:49 +0300

    feat: add database configuration and implement subscription entity with repository and service layers

--- src/main/java/org/example/subscription_tracker/repository/SubscriptionRepository.java
+++ src/main/java/org/example/subscription_tracker/repository/SubscriptionRepository.java
@@ -1,4 +1,10 @@
 package org.example.subscription_tracker.repository;
 
-public class SubscriptionRepository {
+import org.example.subscription_tracker.entity.Subscription;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SubscriptionRepository  extends JpaRepository<Subscription, Long> {
+
 }
--- src/main/java/org/example/subscription_tracker/service/SubscriptionService.java
+++ src/main/java/org/example/subscription_tracker/service/SubscriptionService.java
@@ -1,4 +1,49 @@
 package org.example.subscription_tracker.service;
 
+import org.example.subscription_tracker.entity.Subscription;
+import org.example.subscription_tracker.repository.SubscriptionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class SubscriptionService {
+
+    private SubscriptionRepository subscriptionRepository;
+
+    @Autowired
+    public SubscriptionService(SubscriptionRepository subscriptionRepository) {
+        this.subscriptionRepository = subscriptionRepository;
+    }
+
+    public List<Subscription> findAll() {
+        return subscriptionRepository.findAll();
+    }
+
+    public Optional<Subscription> findById(Long id) {
+        return subscriptionRepository.findById(id);
+    }
+
+    public Subscription save(Subscription subscription) {
+        return subscriptionRepository.save(subscription);
+    }
+
+    public Subscription update(Subscription subscription, Long id) {
+        return subscriptionRepository.findById(id)
+                .map(existing -> {
+                    existing.setServiceName(subscription.getServiceName());
+                    existing.setPrice(subscription.getPrice());
+                    existing.setStartDate(subscription.getStartDate());
+                    existing.setEndDate(subscription.getEndDate());
+                    existing.setAutoRenew(subscription.isAutoRenew());
+                    return subscriptionRepository.save(existing);
+                })
+                .orElseThrow(() -> new RuntimeException("Subscription with ID " + id + " not found"));
+    }
+
+    public void delete(Subscription subscription) {
+        subscriptionRepository.delete(subscription);
+    }
 }
